323b87734311950fb43c4c9cb89d3c684643babc
aladdin <alauddin12340@gmail.com>
2023-01-23 00:12:35 +0600

    create global crypto UI

--- src/App.js
+++ src/App.js
@@ -34,8 +34,19 @@ const App = () => {
                         </Routes>
                     </div>
                 </Layout>
+                <div className="footer">
+                    <Typography.Title level={5} style={{ textAlign: "center" }}>
+                        Copyright © 2021
+                        <Link to="/">Cryptoverse Inc.</Link> <br />
+                        All Rights Reserved.
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
             </div>
-            <div className="footer"></div>
         </div>
     );
 };
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,15 @@
+// const options = {
+//     method: "GET",
+//     url: "https://coinranking1.p.rapidapi.com/exchanges",
+//     params: {
+//         referenceCurrencyUuid: "yhjMzLPhuIDl",
+//         limit: "50",
+//         offset: "0",
+//         orderBy: "24hVolume",
+//         orderDirection: "desc",
+//     },
+//     headers: {
+//         "X-RapidAPI-Key": "50c8d2bca6mshf781ba06a839525p1528a3jsn4a509302333a",
+//         "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+//     },
+// };
