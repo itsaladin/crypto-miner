9782af8e88b7cfaaf565f64a611f2a281974b161
aladdin <alauddin12340@gmail.com>
2023-01-23 02:24:00 +0600

    create redux-toolkit store,called crypto api, data binding

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Typography, Row, Col, Statistic } from "antd";
+import { millify } from "millify";
 import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 
 const Homepage = () => {
+    const { data, isFetching } = useGetCryptosQuery();
+
+    if (isFetching) return "Loading data ...";
+    const globalStats = data?.data?.stats;
+
+    console.log(data);
+
     return (
         <>
             <Title level={2} className="heading">
@@ -12,11 +21,26 @@ const Homepage = () => {
             </Title>
             <Row>
                 <Col span={12}>
-                    <Statistic title="Total Cryptocurrencies" value="5" />
-                    <Statistic title="Total Exchanges" value="5" />
-                    <Statistic title="Total Market Cap" value="5" />
-                    <Statistic title="Total 24H Volume" value="5" />
-                    <Statistic title="Total Markets" value="5" />
+                    <Statistic
+                        title="Total Cryptocurrencies"
+                        value={globalStats.total}
+                    />
+                    <Statistic
+                        title="Total Exchanges"
+                        value={globalStats.totalExchanges}
+                    />
+                    <Statistic
+                        title="Total Market Cap"
+                        value={globalStats.totalMarketCap}
+                    />
+                    <Statistic
+                        title="Total 24H Volume"
+                        value={globalStats.total24hVolume}
+                    />
+                    <Statistic
+                        title="Total Markets"
+                        value={globalStats.totalMarkets}
+                    />
                 </Col>
             </Row>
         </>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,21 @@
-// const options = {
-//     method: "GET",
-//     url: "https://coinranking1.p.rapidapi.com/exchanges",
-//     params: {
-//         referenceCurrencyUuid: "yhjMzLPhuIDl",
-//         limit: "50",
-//         offset: "0",
-//         orderBy: "24hVolume",
-//         orderDirection: "desc",
-//     },
-//     headers: {
-//         "X-RapidAPI-Key": "50c8d2bca6mshf781ba06a839525p1528a3jsn4a509302333a",
-//         "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-//     },
-// };
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    "X-RapidAPI-Key": "50c8d2bca6mshf781ba06a839525p1528a3jsn4a509302333a",
+    "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: "cryptoApi",
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest("/coins"),
+        }),
+    }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
