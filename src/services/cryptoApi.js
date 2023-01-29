9b95fc833d087185e4e4890085c4e7ea79ab0dc2
aladdin <alauddin12340@gmail.com>
2023-01-29 18:06:09 +0600

    create exchange UI

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,6 +15,9 @@ export const cryptoApi = createApi({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`),
         }),
+        getExchanges: builder.query({
+            query: () => createRequest("/exchanges"),
+        }),
         getCryptoDetails: builder.query({
             query: (coinId) => createRequest(`/coin/${coinId}`),
         }),
@@ -31,4 +34,5 @@ export const {
     useGetCryptosQuery,
     useGetCryptoDetailsQuery,
     useGetCryptoHistoryQuery,
+    useGetExchangesQuery,
 } = cryptoApi;
